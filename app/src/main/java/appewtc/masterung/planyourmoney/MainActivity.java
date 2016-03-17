fce13299be3f7fab8eb8663ce4e6deac484c938d
masterUNG <phrombutr@gmail.com>
2016-03-17 14:49:15 +0700

    Check Space Main Finish

--- app/src/main/java/appewtc/masterung/planyourmoney/MainActivity.java
+++ app/src/main/java/appewtc/masterung/planyourmoney/MainActivity.java
@@ -5,17 +5,25 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
     private MyManage myManage;
+    private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
 
         //Request SQLite
         myManage = new MyManage(this);
@@ -24,6 +32,40 @@ public class MainActivity extends AppCompatActivity {
         //testAddValue();
 
     }   // Main Method
+
+    public void clickLogin(View view) {
+
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (userString.equals("") || passwordString.equals("")) {
+            //Have Space
+            myAlert("กรุณากรอกทุกช่อง คะ");
+        } else {
+            //No Space
+            checkAuthen();
+        }
+
+    }   //clickLogin
+
+    private void checkAuthen() {
+
+        //Read data from userTABLE
+
+
+
+    }   // checkAuthen
+
+    private void myAlert(String strAlert) {
+        Toast.makeText(MainActivity.this, strAlert, Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void bindWidget() {
+        userEditText = (EditText) findViewById(R.id.editText5);
+        passwordEditText = (EditText) findViewById(R.id.editText6);
+    }
 
     @Override
     protected void onStart() {
